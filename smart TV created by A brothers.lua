80311b58d61c370a77c8ca10a0830deec2580146
ali71rb-star <ali71rb@gmail.com>
2026-06-08 23:28:05 +0500

    Update smart TV created by A brothers.lua

--- smart TV created by A brothers.lua
+++ smart TV created by A brothers.lua
@@ -5,18 +5,54 @@ pcall(function()
         pcall(function() startup_sound_mp.release() end)
     end
     local MediaPlayer = luajava.bindClass("android.media.MediaPlayer")
+    local File = luajava.bindClass("java.io.File")
     startup_sound_mp = luajava.new(MediaPlayer)
-    startup_sound_mp.setDataSource("/sdcard/解说/Plugins/Smart TV Created By A Brothers/Smart TV Created By A Brothers.mp3")
-    startup_sound_mp.setOnCompletionListener(luajava.createProxy("android.media.MediaPlayer$OnCompletionListener", {
-        onCompletion = function(mediaPlayer)
-            pcall(function() 
-                mediaPlayer.release() 
-                startup_sound_mp = nil
-            end)
+    
+    local sound_path = ""
+    local ext_variants = {"mp3", "aac", "wav", "ogg", "m4a"}
+    local roots = {"/storage/emulated/0/", "/sdcard/"}
+    local target_ext = "Smart TV Created By A Brothers"
+    
+    for _, r in ipairs(roots) do
+        for _, ev in ipairs(ext_variants) do
+            local path_to_test = string.format("%s解说/Plugins/%s/%s.%s", r, target_ext, target_ext, ev)
+            if luajava.new(File, path_to_test).exists() then
+                sound_path = path_to_test
+                break
+            end
         end
-    }))
-    startup_sound_mp.prepare()
-    startup_sound_mp.start()
+        if sound_path ~= "" then break end
+    end
+    
+    if sound_path == "" then
+        pcall(function()
+            local d_path = debug.getinfo(1).source:match("@?(.*)")
+            if d_path and d_path:find("/") then
+                local s_dir = d_path:match("(.+)/[^/]+")
+                for _, ev in ipairs(ext_variants) do
+                    local path_to_test = string.format("%s/%s.%s", s_dir, target_ext, ev)
+                    if luajava.new(File, path_to_test).exists() then 
+                        sound_path = path_to_test 
+                        break
+                    end
+                end
+            end
+        end)
+    end
+    
+    if sound_path ~= "" then
+        startup_sound_mp.setDataSource(sound_path)
+        startup_sound_mp.setOnCompletionListener(luajava.createProxy("android.media.MediaPlayer$OnCompletionListener", {
+            onCompletion = function(mediaPlayer)
+                pcall(function() 
+                    mediaPlayer.release() 
+                    startup_sound_mp = nil
+                end)
+            end
+        }))
+        startup_sound_mp.prepare()
+        startup_sound_mp.start()
+    end
 end)
 -- STARTUP_SOUND_INJECTOR_END
 import "android.widget.*"
